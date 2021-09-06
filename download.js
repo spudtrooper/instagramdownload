a38fdf475781ae12291d755601a4d85addedbdd9
Jeff Palm <jeff@jeffpalm.com>
2021-09-06 14:58:24 -0400

    Update download.js

--- download.js
+++ download.js
@@ -1,14 +1,13 @@
-(function() {
+setInterval(function() {
   document.querySelectorAll('img[class="FFVAD"]').forEach(function(el) {
     console.log(el.getAttribute('alt'));
     let srcset = el.getAttribute('srcset');
     srcset
       .split(',')
       .forEach(function(e) {
-      	let p = e.split(' ');
-      	console.log(p[1] + ' ' + p[0]);
+	let p = e.split(' ');
+	console.log(p[1] + ' ' + p[0]);
       });
   });
-})();
-
-window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+}, 3000);
